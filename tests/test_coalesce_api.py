258518a32202d08434ac967dcce45b3fa429c7e7
Agustin Marquez <agusdmb@gmail.com>
2023-01-29 16:48:06 +0100

    WIP refactor tests

--- tests/test_coalesce_api.py
+++ tests/test_coalesce_api.py
@@ -1,4 +1,6 @@
 import requests_mock
+from hypothesis import example, given
+from hypothesis.strategies import from_regex, integers
 
 from coalesce_api import __version__, coalesce_api
 
@@ -7,33 +9,30 @@ def test_version():
     assert __version__ == "0.1.0"
 
 
-def test_get_health_insurance_details_1():
-    url = "http://api1.com"
-    member_id = "1"
+url_strategy = from_regex(
+    r"https?://[a-zA-Z0-9]+\.[a-zA-Z]+\.(com|org)", fullmatch=True
+)
+
+
+@example(
+    url="http://api1.com", member_id="1", deductible=1000, stop_loss=10000, oop_max=5000
+)
+@example(
+    url="http://api2.com", member_id="2", deductible=1200, stop_loss=13000, oop_max=6000
+)
+@example(
+    url="http://api3.com", member_id="1", deductible=1000, stop_loss=10000, oop_max=6000
+)
+@given(url_strategy, integers(), integers(), integers(), integers())
+def test_get_health_insurance_details(url, member_id, deductible, stop_loss, oop_max):
     with requests_mock.Mocker() as m:
         m.get(
             url,
-            json={"deductible": 1000, "stop_loss": 10000, "oop_max": 5000},
+            json={"deductible": deductible, "stop_loss": stop_loss, "oop_max": oop_max},
         )
         health_insurance_deatails = coalesce_api.get_health_insurance_details(
             url, member_id
         )
         assert health_insurance_deatails == coalesce_api.HealthInsuranceDetails(
-            deductible=1000, stop_loss=10000, oop_max=5000
-        )
-
-
-def test_get_health_insurance_details_2():
-    url = "http://api2.com"
-    member_id = "2"
-    with requests_mock.Mocker() as m:
-        m.get(
-            url,
-            json={"deductible": 1200, "stop_loss": 13000, "oop_max": 6000},
-        )
-        health_insurance_deatails = coalesce_api.get_health_insurance_details(
-            url, member_id
-        )
-        assert health_insurance_deatails == coalesce_api.HealthInsuranceDetails(
-            deductible=1200, stop_loss=13000, oop_max=6000
+            deductible=deductible, stop_loss=stop_loss, oop_max=oop_max
         )
