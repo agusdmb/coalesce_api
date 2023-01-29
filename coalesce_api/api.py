0cbfd2a658bf8acb82c1483e2b43eb85aeae80d7
Agustin Marquez <agusdmb@gmail.com>
2023-01-29 20:06:23 +0100

    error handling on endpoint

--- coalesce_api/api.py
+++ coalesce_api/api.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 
 from coalesce_api import constants, health_insurance
 
@@ -12,4 +12,16 @@ async def ping():
 
 @app.get("/coalesce")
 async def coalesce(member_id: str):
-    return health_insurance.get_coalesce_health_insurance(constants.SOURCES, member_id)
+    try:
+        return health_insurance.get_coalesce_health_insurance(
+            constants.SOURCES, member_id
+        )
+    except health_insurance.HealthInsuranceAPITimeout as e:
+        raise HTTPException(
+            status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail="Source timeout"
+        ) from e
+    except health_insurance.HealthInsuranceAPIValidationError as e:
+        raise HTTPException(
+            status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+            detail="Source data corrupted",
+        ) from e
