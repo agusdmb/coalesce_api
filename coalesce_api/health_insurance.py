9e6f86c67ae12f8e7759bd16193aa385e0949e76
Agustin Marquez <agusdmb@gmail.com>
2023-01-29 16:58:22 +0100

    WIP api timeout

--- coalesce_api/health_insurance.py
+++ coalesce_api/health_insurance.py
@@ -10,9 +10,20 @@ class HealthInsuranceDetails(BaseModel):
     oop_max: int
 
 
+class HealthInsuranceException(Exception):
+    pass
+
+
+class HealthInsuranceAPITimeout(HealthInsuranceException):
+    pass
+
+
 def get_health_insurance_details(url: str, member_id: str) -> HealthInsuranceDetails:
-    response = requests.get(
-        url, params={"member_id": member_id}, timeout=TIMEOUT_SECONDS
-    )
+    try:
+        response = requests.get(
+            url, params={"member_id": member_id}, timeout=TIMEOUT_SECONDS
+        )
+    except requests.exceptions.Timeout as e:
+        raise HealthInsuranceAPITimeout() from e
     health_insurance_details = HealthInsuranceDetails(**response.json())
     return health_insurance_details
