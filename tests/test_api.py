ae3913c9e840f1f360a039f3c1c495c233bd51b9
Agustin Marquez <agusdmb@gmail.com>
2023-01-29 19:28:26 +0100

    test coalesce api

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,8 @@
 import pytest
+import requests_mock
 from fastapi.testclient import TestClient
 
+from coalesce_api import health_insurance
 from coalesce_api.api import app
 
 
@@ -16,6 +18,25 @@ def test_ping(client: TestClient):
 
 
 def test_coalesce_endpoint(client: TestClient):
-    response = client.get("/coalesce", params={"member_id": "1"})
-    assert response.json() == "1"
+    sources = ["http://api1.com", "http://api2.com", "http://api3.com"]
+    with requests_mock.Mocker() as m:
+        m.get(
+            sources[0],
+            json={"deductible": 1000, "stop_loss": 10000, "oop_max": 5000},
+        )
+        m.get(
+            sources[1],
+            json={"deductible": 1200, "stop_loss": 13000, "oop_max": 6000},
+        )
+        m.get(
+            sources[2],
+            json={"deductible": 1000, "stop_loss": 10000, "oop_max": 6000},
+        )
+
+        response = client.get("/coalesce", params={"member_id": "1"})
+
     assert response.status_code == 200
+
+    assert response.json() == health_insurance.HealthInsuranceDetails(
+        deductible=1066, stop_loss=11000, oop_max=5666
+    )
