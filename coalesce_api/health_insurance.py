c9c6524fb255ee96848233cd7aa18a7442921d3b
Agustin Marquez <agusdmb@gmail.com>
2023-01-29 17:26:33 +0100

    WIP average health insurance

--- coalesce_api/health_insurance.py
+++ coalesce_api/health_insurance.py
@@ -1,3 +1,5 @@
+from typing import Sequence
+
 import requests
 from pydantic import BaseModel
 
@@ -27,3 +29,20 @@ def get_health_insurance_details(url: str, member_id: str) -> HealthInsuranceDet
         raise HealthInsuranceAPITimeout() from e
     health_insurance_details = HealthInsuranceDetails(**response.json())
     return health_insurance_details
+
+
+def average_health_insurances(
+    health_insurances: Sequence[HealthInsuranceDetails],
+) -> HealthInsuranceDetails:
+    deductible_avg = 0
+    stop_loss_avg = 0
+    oop_max_avg = 0
+    for health_insurance in health_insurances:
+        deductible_avg += health_insurance.deductible
+        stop_loss_avg += health_insurance.stop_loss
+        oop_max_avg += health_insurance.oop_max
+    return HealthInsuranceDetails(
+        deductible=deductible_avg // len(health_insurances),
+        stop_loss=stop_loss_avg // len(health_insurances),
+        oop_max=oop_max_avg // len(health_insurances),
+    )
